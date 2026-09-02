9c2132159924b85874c1c0d503c9d24d195a7e61
iotecu <161701301+iotecu@users.noreply.github.com>
2026-09-02 15:38:20 +0300

    add firmware safety constants

---
+++ firmware/include/safety.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Safety {
+constexpr bool K1_OFF_IS_3V3 = true;
+constexpr bool K1_ON_IS_24V = true;
+constexpr int CAN_SELECTION_MAX_ACTIVE = 1;
+constexpr bool CABLE_TEST_24V_FORBIDDEN = true;
+constexpr bool MDNS_IS_NON_AUTHORITATIVE = true;
+}
