62c920eeb6a47adefec1cc7797cfb60c0a29f319
iotecu <161701301+iotecu@users.noreply.github.com>
2026-09-02 15:38:14 +0300

    add firmware config constants

---
+++ firmware/include/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace Config {
+constexpr const char* PRODUCT = "BREMSECU G1 REV-2";
+constexpr const char* AP_SSID = "BREMSECU";
+constexpr const char* AP_IP = "192.168.4.1";
+constexpr bool ENABLE_MDNS = false; // optional convenience only
+}
