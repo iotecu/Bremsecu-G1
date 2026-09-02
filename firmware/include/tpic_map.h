efd84e24665b357902df9ec6fd730a090c32f8e4
iotecu <161701301+iotecu@users.noreply.github.com>
2026-09-02 15:38:08 +0300

    add verified TPIC software bit map

---
+++ firmware/include/tpic_map.h
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace TpicBit {
+constexpr int K4_CAN7638_DR = 0;
+constexpr int K5_CAN12098_DR = 1;
+constexpr int K3_CAN12098_CK = 4;
+constexpr int K2_CAN7638_CK = 5;
+constexpr int K1_SELECT_V = 6;
+constexpr int K6_MASTER_GND = 7;
+
+constexpr int OUT17_BALATA = 8;
+constexpr int OUT18_YAYLI = 9;
+constexpr int OUT19_ASANSOR = 10;
+constexpr int OUT20_GND4 = 11;
+constexpr int OUT21_CANH2_DR = 12;
+constexpr int OUT22_CANL2_DR = 13;
+
+constexpr int OUT9_SAG_SINYAL = 16;
+constexpr int OUT10_ARKA_SIS = 17;
+constexpr int OUT11_GND3 = 18;
+constexpr int OUT12_SOL_PARK = 19;
+constexpr int OUT13_SAG_PARK = 20;
+constexpr int OUT14_STOP = 21;
+constexpr int OUT15_GERI = 22;
+constexpr int OUT16_AKU2 = 23;
+
+constexpr int OUT1_AKU1 = 24;
+constexpr int OUT2_KONTAK = 25;
+constexpr int OUT3_GND1 = 26;
+constexpr int OUT4_GND2 = 27;
+constexpr int OUT5_ABS = 28;
+constexpr int OUT6_CANH1_DR = 29;
+constexpr int OUT7_CANL1_DR = 30;
+constexpr int OUT8_SOL_SINYAL = 31;
+}
