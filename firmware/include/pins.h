cc6acaea992adda7e1a4db621c099bc63f80a841
iotecu <161701301+iotecu@users.noreply.github.com>
2026-09-02 15:38:02 +0300

    add verified firmware pin constants

---
+++ firmware/include/pins.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace Pins {
+constexpr int TPIC_SER = 23;
+constexpr int TPIC_CLK = 18;
+constexpr int TPIC_RCK = 5;
+constexpr int TPIC_OE = 13;
+constexpr int MUX_EN = 25;
+constexpr int MUX_S0 = 16;
+constexpr int MUX_S1 = 17;
+constexpr int MUX_S2 = 27;
+constexpr int I2C_SDA = 21;
+constexpr int I2C_SCL = 22;
+constexpr int SAG_PULS = 36;
+constexpr int SOL_PULS = 39;
+constexpr int SD_MISO = 34;
+constexpr int SD_SCK = 32;
+constexpr int SD_MOSI = 33;
+constexpr int SD_CS = 4;
+}
